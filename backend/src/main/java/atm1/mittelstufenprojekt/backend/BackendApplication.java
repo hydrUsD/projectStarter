00c4af44ff25afa64f8919f545b52642c46af791
hydrUsD <proletsfail@gmail.com>
2025-09-30 13:26:40 +0200

    feat(backend): Spring Boot skeleton with CORS, health & CI

---
+++ backend/src/main/java/atm1/mittelstufenprojekt/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package atm1.mittelstufenprojekt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class,
+	HibernateJpaAutoConfiguration.class
+})
+public class BackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
